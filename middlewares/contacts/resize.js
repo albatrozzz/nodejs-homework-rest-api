505a0347b196a751af9fa88bdef04285cbdd840a
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2022-02-14 07:42:36 +0200

    Update resize.js

--- middlewares/contacts/resize.js
+++ middlewares/contacts/resize.js
@@ -2,10 +2,7 @@
 const Jimp = require('jimp')
 
     const resize = (req, res, next) => {
-        console.log(req.file)
-        if(!req.file){
-            console.log("It's a trap!")
-        }
+
         Jimp.read(req.file.path, (error, file) => {
             if (error){
             throw new createError(400, error.message)
