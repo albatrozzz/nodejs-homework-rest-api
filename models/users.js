87cbce19214e0363eb28c0a7954e9456e7d14fbb
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2022-02-19 14:21:29 +0200

    выполнено задание согласно условия

--- models/users.js
+++ models/users.js
@@ -4,27 +4,35 @@ const Joi = require("joi")
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = Schema({
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-      },
-      email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-      },
-      subscription: {
-        type: String,
-        enum: ["starter", "pro", "business"],
-        default: "starter"
-      },
-      token: {
-        type: String,
-        default: null,
-      },
-      avatarURL: {
-        type: String
-    }
+  password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    avatarURL: {
+      type: String
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
 }, {versionKey: false, timestamps: true})
 
 const User = model("user", userSchema)
@@ -32,6 +40,10 @@ const User = model("user", userSchema)
 const registerJoiSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
+})
+
+const emailJoiSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp)
 })
 
 const updateSubscriptionJoiSchema = Joi.object({
@@ -42,4 +54,5 @@ module.exports = {
     User,
     auth: registerJoiSchema,
     update: updateSubscriptionJoiSchema,
+    emailValidation: emailJoiSchema
 }
